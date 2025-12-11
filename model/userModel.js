91c2f0a2143900ca69142b65c13d975c1943e34f
soumencsit <sp01csit@gmail.com>
2025-12-11 21:19:54 +0530

    Update mongo URI

--- model/userModel.js
+++ model/userModel.js
@@ -27,10 +27,7 @@ const userSchema = new mongoose.Schema({
     required: true,
     minlength: 6, 
   },
-  // isVerified: {
-  //   type: Boolean,
-  //   default: false,
-  // },
+
   createdAt: {
     type: Date,
     default: Date.now,
